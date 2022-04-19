8e8f5c96c0a4e08b5de6762596b1ffb395c91f0c
Carolyn Webster-Pless <99274537+CarolynWP@users.noreply.github.com>
2022-04-19 19:42:11 +0000

    soup works also I finished it :)

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 "use strict"
 
 function calculate() {
-	
+	//variable
+let radius= parseFloat(document.getElementById('radius').value);
+	//calculation
+let volume=4/3*Math.PI*radius**3
+	//output
+	document.getElementById("answer").innerHTML= "The volume of your sphere is " + volume.toFixed(2)+"cm³."
 }
